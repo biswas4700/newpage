05fcdffac82f154d04037e8a9018a2c08392485f
biswas4700 <129825791+biswas4700@users.noreply.github.com>
2023-04-11 17:34:01 +0600

    Za

--- link.js
+++ link.js
@@ -1,2 +1,2 @@
 const backupLink = "http://www.titrk.com/SHz5p";
-const mainLink = "";
+const mainLink = "https://rgordg.palatlaldate.com?utm_source=da57dc555e50572d&s1=147255&s2=1736809&s3=Live&s5=Live&click_id=Live&j1=1";
