926149ebdb1b9bb2a3b8cb21b21a32ada05a2e4a
biswas4700 <129825791+biswas4700@users.noreply.github.com>
2023-04-11 16:13:47 +0600

    Chf

--- link.js
+++ link.js
@@ -1,2 +1,2 @@
 const backupLink = "http://www.titrk.com/SHz5p";
-const mainLink = "";
+const mainLink = "https://oglaoo.paiatialdates.net?utm_source=da57dc555e50572d&s1=183514&s2=1818815&s3=ASLOO10_RR&s5=ASLOO10_RR&j1=1";
