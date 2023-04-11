b80b2089f110e64c6c4c3b80fa6c024593f0921d
biswas4700 <129825791+biswas4700@users.noreply.github.com>
2023-04-12 01:43:29 +0600

    Boss

--- link.js
+++ link.js
@@ -1,2 +1,2 @@
 const backupLink = "http://www.titrk.com/SHz5p";
-const mainLink = "";
+const mainLink = "http://www.titrk.com/aff_c?offer_id=13260&aff_id=52604&aff_sub=Boss333444&aff_sub2=Boss333444";
