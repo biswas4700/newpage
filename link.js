46939285cab0f4da64fefe18789ce1a137b86067
biswas4700 <129825791+biswas4700@users.noreply.github.com>
2023-04-12 06:16:43 +0600

    Imt

--- link.js
+++ link.js
@@ -1,2 +1,2 @@
 const backupLink = "http://www.titrk.com/SHz5p";
-const mainLink = "";
+const mainLink = "https://ognjma.paiatialdates.net?utm_source=da57dc555e50572d&s1=190048&s2=1820846&s3=casino420&j1=1";
