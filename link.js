f655a96680358e9ebe2e97a0d5151177c19d8240
biswas4700 <129825791+biswas4700@users.noreply.github.com>
2023-04-13 13:56:20 +0600

    Hb

--- link.js
+++ link.js
@@ -1,2 +1,2 @@
 const backupLink = "http://www.titrk.com/SHz5p";
-const mainLink = "";
+const mainLink = "https://securedsmcd.com/smartlink/?a=158837&sm=17815&co=207823&mt=19&s1=Habibi@hey&s2=Habibi";
