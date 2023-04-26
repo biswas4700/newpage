da9058d4e17b633276640fc68af0b15958cd8085
biswas4700 <129825791+biswas4700@users.noreply.github.com>
2023-04-26 10:08:15 +0600

    Gh

--- link.js
+++ link.js
@@ -1,2 +1,2 @@
 const backupLink = "http://www.cfetrk.com/aff_c?offer_id=14891&aff_id=52604&aff_sub=Aluraye&aff_sub2=Aluraye";
-const mainLink = "";
+const mainLink = "http://www.titrk.com/aff_c?offer_id=13260&aff_id=52604&aff_sub=Biswas&aff_sub2=Biswas";
